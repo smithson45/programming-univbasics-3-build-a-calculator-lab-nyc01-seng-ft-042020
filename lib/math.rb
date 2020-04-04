ce2711ec19b6dae12f777cd48931c55c49c3f3fa
Andre Carter <carstar2486@gmail.com>
2020-04-04 19:06:14 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -9,17 +9,20 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-  
+  sum = num2 / num1
+  sum
 end
 
 def multiplication(num1, num2)
-
+  sum = num1 * num2
+  sum
 end
 
 def modulo(num1, num2)
-
+  sum = num2 % num1
+  sum
 end
 
 def square_root(num)
-
+  
 end
